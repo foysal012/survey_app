4d1bfe6164a6347034d0e0bfbb0cc802ca4500f6
Foysal <foysaljoarder012@gmail.com>
2025-07-25 16:03:57 +0600

    Rewarded Add implementation done

--- lib/services/ad_helper.dart
+++ lib/services/ad_helper.dart
@@ -21,4 +21,14 @@ class AdHelper {
       throw UnsupportedError('Unsupported platform');
     }
   }
+
+  static String get rewardedAdUnitId {
+    if(Platform.isAndroid){
+      return 'ca-app-pub-9093812169385004/8208500728';
+    } else if( Platform.isIOS) {
+      return '<Your_IOS_BANNER_AD_ID>';
+    } else {
+      throw UnsupportedError('Unsupported platform');
+    }
+  }
 }
